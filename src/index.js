361731a2ffd86a16b25099196841a6e6add68fb1
Ramon Santos <157428663+Ramon-Eduardo@users.noreply.github.com>
2025-05-07 21:20:25 -0300

    minha-contribuição

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@
 const readline = require('readline-sync');
 
 // Capturando o número de pessoas na mesa
-const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
+const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa:');
 
 // Capturando o valor total da conta
 const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
 
 // Capturando o método de pagamento
-const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
-
-
+const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)?');
 //Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
-
+let valorFinal;
+    if (metodoPagamento === "PIX" || metodoPagamento === "dinheiro") {
+    valorFinal = valorTotal * 0.9 / numeroPessoas;        
+} else {(metodoPagamento === "cartão") 
+    valorFinal = valorTotal / numeroPessoas;
+} 
 
 // Exibindo os resultados
+console.log(valorFinal, "pra cada")
